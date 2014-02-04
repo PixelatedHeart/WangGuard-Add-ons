e0000c3b52af81a1f04d0cbac556cd6d6e2db0c6
José Conti <j.conti@joseconti.com>
2014-02-04 23:50:49 +0100

    Added compatibility with BuddyPress

--- wangguard-limited-registration-domain-add-on/wangguard-limited-registration-domain-bp.php
+++ wangguard-limited-registration-domain-add-on/wangguard-limited-registration-domain-bp.php
@@ -11,11 +11,9 @@ function wangguard_limit_domain_registration_bp_blocked_add_on(){
 		global $bp;
 		
 		$signup_email = $_REQUEST['signup_email'];
-        
         $blocked = wangguard_is_domain_blocked_add_on($signup_email);
-		
-		if ($blocked) {
-			$bp->signup->errors['signup_email'] = wangguard_fix_bp_slashes_maybe( __("<strong>ERROR</strong>: Domain not allowed.", 'wangguard'));
+   		if ($blocked) {
+				$bp->signup->errors['signup_email'] = wangguard_fix_bp_slashes_maybe( __("<strong>ERROR</strong>: Domain not allowed.", 'wangguard'));
         }     
         if (isset ($bp->signup->errors['signup_email']))$bp->signup->errors['signup_email'] = wangguard_fix_bp_slashes_maybe($bp->signup->errors['signup_email']);  
 }
@@ -23,16 +21,16 @@ function wangguard_limit_domain_registration_bp_blocked_add_on(){
 function wangguard_limit_domain_registration_allowed_bp_add_on(){
 		global $bp;
         
-        $signup_email = $_REQUEST['signup_email'];
-        
-        $allowed = wangguard_is_domain_allowed_add_on($signup_email);
-		
-		if ($allowed == false) {
-			$bp->signup->errors['signup_email'] = wangguard_fix_bp_slashes_maybe(__("<strong>ERROR</strong>: Domain not allowed Limitación a uno.", 'wangguard'));
-        }
-        if (isset ($bp->signup->errors['signup_email']))$bp->signup->errors['signup_email'] = wangguard_fix_bp_slashes_maybe($bp->signup->errors['signup_email']);      
+        $domains = array_filter(get_site_option( 'wangguard_limited_email_domains'));
+       		if (!empty($domains)){
+		   		$signup_email = $_REQUEST['signup_email'];
+		   		$allowed = wangguard_is_domain_allowed_add_on($signup_email);
+		   		if ($allowed == false) {
+					$bp->signup->errors['signup_email'] = wangguard_fix_bp_slashes_maybe(__("<strong>ERROR</strong>: Domain not allowed.", 'wangguard'));
+				}
+				if (isset ($bp->signup->errors['signup_email']))$bp->signup->errors['signup_email'] = wangguard_fix_bp_slashes_maybe($bp->signup->errors['signup_email']);      
+			}
 }
-//add_action('wangguard_wp_signup_validate', 'wangguard_limit_domain_regisration_blocked_allowed_add_on');
 add_action('bp_signup_validate', 'wangguard_limit_domain_registration_bp_blocked_add_on',10,3);
 add_action('bp_signup_validate', 'wangguard_limit_domain_registration_allowed_bp_add_on',10,3);
 ?>
