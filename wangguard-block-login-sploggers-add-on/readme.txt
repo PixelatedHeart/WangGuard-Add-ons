cbf00fddde5f02a056cc1338bd99d908fa0ed83d
José Conti <j.conti@joseconti.com>
2014-04-26 11:52:35 +0200

    Uploading new add-on
    Uploading new add-on WangGuard Block Login to Sploggers
    
    With this add-on (only for WordPress non multisite, the sploggers will
    be blocked if they try to login to WordPress.

---
+++ wangguard-block-login-sploggers-add-on/readme.txt
@@ -0,0 +1,49 @@
+=== WangGuard Block login to Sploggers Add-On ===
+Contributors: j.conti
+Author URI: http://wangguard.com
+Tags: wangguard, wgg, wangguardaddon, block login, login, sploggers, spam users
+Requires at least: 3.8
+Tested up to: 3.9
+Stable tag: 1.0.0
+License: GPLv2
+
+With WangGuard Block Login to Sploggers Add-On, you not allow to login to sploggers detected by WangGuard. Only works with WordPress non Multisite.
+
+
+== Description ==
+
+With this WangGuard Add-on, the sploggers detected ato your WordPress non Multisite will be blocked to login.
+
+This add-on is not necesarie for WordPress Multisite or BuddyPress, becasuse the Sploger detected are marked as splogers and Spam users and WordPress Multisite and BuddyPress have ist own block sustem.
+
+[WangGuard plugin](http://wordpress.org/extend/plugins/wangguard) version 1.6+ is needed
+
+= Why use WangGuard Block login to Sploggers Add-On? =
+
+If you own a WordPress non multiste with WangGuard, and you don remove the sploggers, with this add-on, the sploggers will be blocked if they try to login. This add-on will help you to protect from Sploggers. If you are a WordPress non multiste owner, and you use WangGuard, this is a must have add-on.
+
+
+== Installation ==
+
+1. Download and install WangGuard Block login to Sploggers Add-On
+
+2. Activate plugins through the "Plugins" menu in WordPress (if is a Multisite, WangGuard needs to be activated for the Network)
+
+3. Updates are automatic. Click on "Upgrade Automatically" if prompted from the admin menu. If you ever have to manually upgrade, simply deactivate, uninstall, and repeat the installation steps with the new version. 
+
+
+
+== Screenshots ==
+
+No screenshots
+
+== Frequently Asked Questions ==
+
+= How to use it? =
+
+No setting needed at this time
+== Changelog ==
+
+= 1.0.0 - 22 April 2014 =
+* Initial Release
+
---
+++ wangguard-block-login-sploggers-add-on/wangguard-block-login-sploggers-add-on.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: WangGuard Block login Sploggers Add-On
+Plugin URI: http://www.wangguard.com
+Description: Limit registration to certains domains or block certains domains . WangGuard plugin version 1.6 or higher is required, download it for free from <a href="http://wordpress.org/extend/plugins/wangguard/">http://wordpress.org/extend/plugins/wangguard/</a>.
+Version: 1.0.0
+Author: WangGuard
+Author URI: http://www.wangguard.com
+License: GPL2
+*/
+
+/*  Copyright 2012  WangGuard (email : info@wangguard.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+define('WANGGUARD_BLOCK_LOGIN_SPLOGGERS_VERSION', '1.0');
+
+function wangguard_block_login_sploggers_init() {
+
+if (function_exists('load_plugin_textdomain')) {
+		$plugin_dir = basename(dirname(__FILE__));
+		load_plugin_textdomain('wangguard-block-login-sploggers-add-on', false, $plugin_dir . "/languages/" );
+	}
+}
+add_action('init', 'wangguard_block_login_sploggers_init');
+
+
+function wangguard_block_login_sploggers_notices() {
+	if ( !defined('WANGGUARD_VERSION') ) {
+		echo "
+		<div  class='error fade'><p><strong>".__('WangGuard Block Login Sploggers Add-On is almost ready.', 'wangguard-registration-add-on')."</strong> ". __('You must install and activate <a href="http://wordpress.org/extend/plugins/wangguard/">WangGuard</a> 1.6-RC1 or higher to use this plugin.', 'wangguard-registration-add-on')."</p></div>
+		";
+	}
+	else {
+		if ( defined('WANGGUARD_VERSION') ) {$version = WANGGUARD_VERSION;}
+		if ($version)
+		if (version_compare($version , '1.6-RC1') == -1)
+			echo "
+			<div  class='error fade'><p><strong>".__('WangGuard Block Login Sploggers Add-On is almost ready.', 'wangguard-registration-add-on')."</strong> ". __('You need to upgrade <a href="http://wordpress.org/extend/plugins/wangguard/">WangGuard</a> to version 1.6-RC1 or higher to use this plugin.', 'wangguard-registration-add-on')."</p></div>
+			";
+	}
+}
+add_action('admin_notices', 'wangguard_block_login_sploggers_notices');
+
+
+//Check user status
+function wangguard_look_user_status($userid){
+			global $wpdb;
+
+			$table_name = $wpdb->base_prefix . "wangguarduserstatus";
+			$status = $wpdb->get_var( $wpdb->prepare("select user_status from $table_name where ID = %d" , $userid) );
+			
+			return $status;
+                    
+}
+
+
+
+/********************************************************************/
+/*** CHECK SPLOGGER USER IN THE WORDPRESS LOGIN FORM BEGINS **/
+/********************************************************************/
+
+function wangguard_check_user_login( $user, $username, $password ) {    
+    
+    $user = get_user_by( 'login', $username );
+    $userid = $user->ID;
+    if ($userid) $status = wangguard_look_user_status($userid);
+    
+    if ( $status == 'reported' ) {
+        return new WP_Error(1, 'You are not allowed to login.<br />Contact with the webmaster');
+    }
+    return $user;
+}
+add_filter( 'authenticate', 'wangguard_check_user_login', 30, 3 );
+
+?>
